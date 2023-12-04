f8d9def41caa3cb405325571ac36ed948e8b6e00
saklani <50768838+saklani@users.noreply.github.com>
2023-12-04 15:56:05 +0530

    simple signer

--- src/signer.ts
+++ src/signer.ts
@@ -1,23 +1,24 @@
 import Arweave from 'arweave';
 import { JWKInterface } from 'arweave/node/lib/wallet';
+import { jsonToJWKInterface } from './utils';
+import Transaction from 'arweave/node/lib/transaction';
 
 export default class Signer {
 
-  private privateKey: JWKInterface;
+  private jwk: JWKInterface;
   public arweave: Arweave;
 
-  constructor(privateKey: JWKInterface) {
-    this.privateKey = privateKey;
-    this.arweave = Arweave.init({
-
-    });
+  constructor(privateKey: string | object | Record<string, string> | JWKInterface) {
+    this.jwk = jsonToJWKInterface(privateKey);
+    this.arweave = Arweave.init({});
   }
 
-  async sign() {
+  async sign(transaction: Transaction): Promise<void> {
+    return this.arweave.transactions.sign(transaction, this.jwk);
   }
 
-  async getPublicAddress() {
-    return this.arweave.wallets.getAddress();
+  async getPublicAddress(): Promise<string> {
+    return this.arweave.wallets.getAddress(this.jwk);
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
-export function jsonToJWKInterface() {
-  
+import { JWKInterface } from "arbundles/node";
+
+export function jsonToJWKInterface(key: Record<string, string> | string | object): JWKInterface {
+  return JSON.parse(key.toString());
 }
